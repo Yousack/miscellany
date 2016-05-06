49dc9e10f14e0af856bc23a5d0228feb5e0a6ac8
soukouki <soukouki0@yahoo.co.jp>
2016-05-06 17:25:13 +0900

    ナベアツにワン!とアホワン!を追加

--- nabeatsu.rb
+++ nabeatsu.rb
@@ -1,9 +1,14 @@
 n = gets.to_i
 
 n.times do |i|
-  if (i+1)%3 == 0 || (i+1).to_s.include?("3")
-    print("#{i+1}!! ")
+
+  if (i+1)%15 == 0
+    puts("#{i+1}アホワン!")
+  elsif (i+1)%5 == 0
+    puts("#{i+1}ワン")
+  elsif (i+1)%3 == 0 || (i+1).to_s.include?("3")
+    puts("#{i+1}アホ!")
   else
-    print("#{i+1} ")
+    puts("#{i+1}")
   end
 end
